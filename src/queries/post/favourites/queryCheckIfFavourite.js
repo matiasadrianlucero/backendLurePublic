1301eaea41579e8dfac5c9f600310036641c3ccd
matiasadrianlucero <108703587+matiasadrianlucero@users.noreply.github.com>
2025-08-12 05:35:46 -0300

    Update queryCheckIfFavourite.js

--- src/queries/post/favourites/queryCheckIfFavourite.js
+++ src/queries/post/favourites/queryCheckIfFavourite.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
-export async function querycheckifFavourite(postIdCheck,userIdCheck){
+export async function querycheckIfFavourite(postIdCheck,userIdCheck){
     try {
         const checkFavourite = await prisma.favourites.findMany({
             where: {
